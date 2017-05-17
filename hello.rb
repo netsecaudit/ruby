51482653ea634156118c677f7832459d41e3d0d8
netsecaudit <netsecaudit@gmail.com>
2017-05-17 11:53:04 +0800

    second
    update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,1 +1,15 @@
-helloword
+def say_hello1(name)  
+  return "Hello , "+ name    
+end  
+  
+def say_hello2 name  
+  return "Hello , " + name  
+end  
+  
+def say_hello3 name  
+  return "Hello , #{name}"  
+end  
+  
+puts say_hello1 "lql1"  
+puts say_hello2 "lql2"  
+puts say_hello3 "lql3"  
