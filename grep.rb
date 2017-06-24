f93967f466c0cb73de8cd5a4c9c809e961be067a
netsecaudit <netsecaudit@gmail.com>
2017-06-24 10:07:05 +0800

    commit

--- grep.rb
+++ grep.rb
@@ -1,35 +1,26 @@
 filename = ARGV[0]
-     key = ARGV[1]
-     num = 1
-if filename==nil||key==nil
+key = ARGV[1]
+num = 1
+
+if filename.nil? || key.nil?
   puts "usage: grep [filepath] [keyword]"
   exit
 end
+
 file=File.open(filename)
 file.each_line do |line|
-  if /#{key.downcase}/ =~ line
+    if /#{key.upcase}/ =~ line || /#{key.downcase}/ =~ line || /#{key}/ =~ line
     print "line:",num," "
     line.split.each do |word|
-     if key.downcase==word
+     if key.upcase==word || key.downcase==word || key==word
         print "\033[45m #{word} \033[0m "
       else
         print word," "
-    end
-    end
-    print "\n"
-  else
-  if /#{key.upcase}/ =~ line
-    print "line:",num," "
-    line.split.each do |word|
-      if key.upcase==word
-        print "\033[45m #{word} \033[0m "
-      else
-        print word," "
-      end
+     end
     end
     print "\n"
-  end
-  end
-  num=num+1
 end
+num = num+1
+end
+
 file.close
