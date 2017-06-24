45c47c06f01aa1adf74934c1f96922366384fe18
netsecaudit <netsecaudit@gmail.com>
2017-06-24 10:12:36 +0800

    commit

--- grep.rb
+++ grep.rb
@@ -12,14 +12,14 @@ file.each_line do |line|
     if /#{key.upcase}/ =~ line || /#{key.downcase}/ =~ line || /#{key}/ =~ line
     print "line:",num," "
     line.split.each do |word|
-     if key.upcase==word || key.downcase==word || key==word
+      if key.upcase==word || key.downcase==word || key==word
         print "\033[45m #{word} \033[0m "
       else
         print word," "
-     end
+      end
     end
     print "\n"
-end
+  end
 num = num+1
 end
 
